0851f70d26440ebf8e48f07429c4406208dc11a2
joywu001 <joywu001@gmail.com>
2022-12-02 16:02:01 +0800

    功能頁+測試結果頁面置中完成

--- app/src/main/java/com/example/demo/Skin_Discuss.java
+++ app/src/main/java/com/example/demo/Skin_Discuss.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.demo.R;
 
 public class Skin_Discuss extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
